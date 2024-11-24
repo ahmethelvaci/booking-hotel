3c6918d485df2051b4da4af985e8db1c39d34bcb
Ahmet Helvacı <ahmethelv@gmail.com>
2024-11-24 21:31:39 +0300

    implement Databases/HotelRepository

--- app/Providers/HotelServiceProvider.php
+++ app/Providers/HotelServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use App\Contracts\Repositories\HotelRepository as HotelRepositoryContract;
 use App\Contracts\Services\HotelService as HotelServiceContract;
-use App\Repositories\Databases\HotelRepository as HotelDatabasesRepository;
+use App\Repositories\Databases\HotelRepository as HotelDatabaseRepository;
 use App\Repositories\Eloquents\HotelRepository as HotelEloquentRepository;
 use App\Services\HotelService;
 use Illuminate\Support\ServiceProvider;
